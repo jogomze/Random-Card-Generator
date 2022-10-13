bb9a1ee33bc90815ae05b7210756fade009fd5ce
Jose Gomez <jose.gomezpereira001@mymdc.net>
2022-10-13 00:41:34 +0000

    almost done card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
-/* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add("heart")
+  setTimeout(() => {
+    document.querySelector(".card").classList.remove("heart");
+    document.querySelector(".card").classList.add("spade");
+  }, 1000);
 };
